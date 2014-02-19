0fb9a9b766fa364f3c9e307ef4298307bbb0b6e6
Arnav Gupta <championswimmer@aokp.co>
2014-02-20 00:37:42 +0530

    lib: change names of arguments of ReachUsFragment

--- libeventful/src/main/java/in/tosc/eventful/fragments/reachus/ReachUsFragment.java
+++ libeventful/src/main/java/in/tosc/eventful/fragments/reachus/ReachUsFragment.java
@@ -18,28 +18,28 @@ import in.tosc.eventful.R;
 public class ReachUsFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_LAT = "rlat";
+    private static final String ARG_LONG = "rlong";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String mLat;
+    private String mLong;
 
 
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param rlat Parameter 1.
+     * @param rlong Parameter 2.
      * @return A new instance of fragment ReachUsFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ReachUsFragment newInstance(String param1, String param2) {
+    public static ReachUsFragment newInstance(String rlat, String rlong) {
         ReachUsFragment fragment = new ReachUsFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(ARG_LAT, rlat);
+        args.putString(ARG_LONG, rlong);
         fragment.setArguments(args);
         return fragment;
     }
@@ -52,8 +52,8 @@ public class ReachUsFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            mLat = getArguments().getString(ARG_LAT);
+            mLong = getArguments().getString(ARG_LONG);
         }
     }
 
