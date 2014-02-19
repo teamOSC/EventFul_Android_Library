ac1339cf6bf90224b4947a07a2d7136a5c758d2d
Arnav Gupta <championswimmer@aokp.co>
2014-02-20 00:38:42 +0530

    app: add demo code

--- sampleApp/src/main/java/in/tosc/eventful/sampleapp/MainActivity.java
+++ sampleApp/src/main/java/in/tosc/eventful/sampleapp/MainActivity.java
@@ -1,6 +1,11 @@
 package in.tosc.eventful.sampleapp;
 
+import android.app.Fragment;
+
 import in.tosc.eventful.activities.MainDrawerActivity;
+import in.tosc.eventful.fragments.AboutFragment;
+import in.tosc.eventful.fragments.ContactFragment;
+import in.tosc.eventful.fragments.reachus.ReachUsFragment;
 
 /**
  * Created by championswimmer on 18/2/14.
@@ -9,6 +14,15 @@ public class MainActivity extends MainDrawerActivity {
 
     @Override
     public void setTopFragments() {
+        topFragments = new Fragment[]{
+                AboutFragment.newInstance("A", "A"),
+                ContactFragment.newInstance("a", "a")
+        };
+        topFragmentNames = new String[]{
+                "About",
+                "Contact"
+        };
+
 
     }
 }
