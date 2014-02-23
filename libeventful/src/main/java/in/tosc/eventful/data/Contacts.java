c37509b9c58b8879a1f46c38a9499694d6e26675
Arnav Gupta <championswimmer@aokp.co>
2014-02-24 04:34:19 +0530

    lib: add class to set and get contact details

---
+++ libeventful/src/main/java/in/tosc/eventful/data/Contacts.java
@@ -0,0 +1,30 @@
+package in.tosc.eventful.data;
+
+import android.content.Context;
+
+/**
+ * Created by championswimmer on 24/2/14.
+ */
+public class Contacts {
+
+    private Context c;
+    private String[] contactNames = {""};
+    private String[] contactEmails = {""};
+
+    public void setContactNames (int resId) {
+        contactNames = c.getResources().getStringArray(resId);
+    }
+
+    public void setContactEmails (int resId) {
+        contactEmails = c.getResources().getStringArray(resId);
+    }
+
+    public String[] getContactNames () {
+        return contactNames;
+    }
+
+    public String[] getContactEmails () {
+        return contactEmails;
+    }
+
+}
