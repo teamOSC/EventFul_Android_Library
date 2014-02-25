b98eb9e768357375bb82dcedfbeacd11840ac3e4
Arnav Gupta <championswimmer@aokp.co>
2014-02-25 12:31:54 +0530

    lib : add EventfulEvent data object

---
+++ libeventful/src/main/java/in/tosc/eventful/data/EventfulEvent.java
@@ -0,0 +1,60 @@
+package in.tosc.eventful.data;
+
+import android.content.Context;
+
+/**
+ * Created by championswimmer on 25/2/14.
+ */
+public class EventfulEvent {
+
+    private Context c;
+
+    private String[] eventNames = {""};
+    private String[] eventDescs = {""};
+    private String[] eventDates = {""};
+    private String[] eventStartTimes = {""};
+    private String[] eventEndTimes = {""};
+    private String[] eventContacts = {""};
+
+    public EventfulEvent (Context context) {
+        c = context;
+    }
+
+    public void setEventNames (int resId) {
+        eventNames = c.getResources().getStringArray(resId);
+    }
+    public void setEventDescs (int resId) {
+        eventDescs = c.getResources().getStringArray(resId);
+    }
+    public void setEventDates (int resId) {
+        eventDates = c.getResources().getStringArray(resId);
+    }
+    public void setEventStartTimes (int resId) {
+        eventStartTimes = c.getResources().getStringArray(resId);
+    }
+    public void setEventEndTimes (int resId) {
+        eventEndTimes = c.getResources().getStringArray(resId);
+    }
+    public void setEventContacts (int resId) {
+        eventContacts = c.getResources().getStringArray(resId);
+    }
+
+    public String[] getEventNames () {
+        return eventNames;
+    }
+    public String[] getEventDescs () {
+        return eventDescs;
+    }
+    public String[] getEventDates () {
+        return eventDates;
+    }
+    public String[] getEventStartTimes () {
+        return eventStartTimes;
+    }
+    public String[] getEventEndTimes () {
+        return eventEndTimes;
+    }
+    public String[] getEventContacts () {
+        return eventContacts;
+    }
+}
