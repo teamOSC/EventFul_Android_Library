d65e2a8dda11ccd68620280b3d9aa715c8273ac9
Arnav Gupta <championswimmer@aokp.co>
2014-03-07 00:45:59 +0530

    update docs
    Signed-off-by: Arnav Gupta <championswimmer@aokp.co>

---
+++ libeventful/assets/navtree_data.js
@@ -0,0 +1,27 @@
+var NAVTREE_DATA =
+[ [ "com.astuetz", "com/astuetz/package-summary.html", [ [ "Interfaces", null, [ [ "PagerSlidingTabStrip.IconTabProvider", "com/astuetz/PagerSlidingTabStrip.IconTabProvider.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "PagerSlidingTabStrip", "com/astuetz/PagerSlidingTabStrip.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "in.tosc.eventful", "in/tosc/eventful/package-summary.html", [ [ "Classes", null, [ [ "EventfulActivity", "in/tosc/eventful/EventfulActivity.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "in.tosc.eventful.activities", "in/tosc/eventful/activities/package-summary.html", [ [ "Interfaces", null, [ [ "NavigationDrawerFragment.NavigationDrawerCallbacks", "in/tosc/eventful/activities/NavigationDrawerFragment.NavigationDrawerCallbacks.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "MainDrawerActivity", "in/tosc/eventful/activities/MainDrawerActivity.html", null, "" ], [ "MainSwipeTabActivity", "in/tosc/eventful/activities/MainSwipeTabActivity.html", null, "" ], [ "MainSwipeTabActivity.SectionsPagerAdapter", "in/tosc/eventful/activities/MainSwipeTabActivity.SectionsPagerAdapter.html", null, "" ], [ "NavigationDrawerFragment", "in/tosc/eventful/activities/NavigationDrawerFragment.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "in.tosc.eventful.data", "in/tosc/eventful/data/package-summary.html", [ [ "Classes", null, [ [ "Contacts", "in/tosc/eventful/data/Contacts.html", null, "" ], [ "EventfulEvent", "in/tosc/eventful/data/EventfulEvent.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "in.tosc.eventful.fragments", "in/tosc/eventful/fragments/package-summary.html", [ [ "Classes", null, [ [ "AboutFragment", "in/tosc/eventful/fragments/AboutFragment.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "in.tosc.eventful.fragments.contacts", "in/tosc/eventful/fragments/contacts/package-summary.html", [ [ "Classes", null, [ [ "ContactFragment", "in/tosc/eventful/fragments/contacts/ContactFragment.html", null, "" ], [ "ContactGoogleCardFragment", "in/tosc/eventful/fragments/contacts/ContactGoogleCardFragment.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "in.tosc.eventful.fragments.events", "in/tosc/eventful/fragments/events/package-summary.html", [ [ "Classes", null, [ [ "EventDaySliderFragment", "in/tosc/eventful/fragments/events/EventDaySliderFragment.html", null, "" ], [ "EventItemSliderFragment", "in/tosc/eventful/fragments/events/EventItemSliderFragment.html", null, "" ], [ "EventSingleItemFragment", "in/tosc/eventful/fragments/events/EventSingleItemFragment.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "in.tosc.eventful.fragments.reachus", "in/tosc/eventful/fragments/reachus/package-summary.html", [ [ "Classes", null, [ [ "ReachUsFragment", "in/tosc/eventful/fragments/reachus/ReachUsFragment.html", null, "" ], [ "ReachUsStaticFragment", "in/tosc/eventful/fragments/reachus/ReachUsStaticFragment.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "in.tosc.eventful.fragments.register", "in/tosc/eventful/fragments/register/package-summary.html", [ [ "Classes", null, [ [ "RegisterInAppFragment", "in/tosc/eventful/fragments/register/RegisterInAppFragment.html", null, "" ], [ "RegisterWebViewFragment", "in/tosc/eventful/fragments/register/RegisterWebViewFragment.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "in.tosc.eventful.utils", "in/tosc/eventful/utils/package-summary.html", [ [ "Classes", null, [ [ "GoogleCardsAdapter", "in/tosc/eventful/utils/GoogleCardsAdapter.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
