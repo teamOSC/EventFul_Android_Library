26ce27834d993154f424c87b50d4e5b95a0d9572
Arnav Gupta <championswimmer@aokp.co>
2014-03-07 01:08:10 +0530

    add javadoc generation tools
    Signed-off-by: Arnav Gupta <championswimmer@aokp.co>

---
+++ javadoc.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#defaults
+###########################################################
+THIS=$(readlink -f "$0")
+HERE=$(dirname "$THIS")
+OUTDIR="/home/championswimmer/android/eventful_docs"
+STYLESHEET=""
+DOCLETPATH=""
+DOCLET=""
+EXTRA1=""
+EXTRA2=""
+###########################################################
+
+
+#customs (only uncomment one doclet)
+
+### doclet: doclava ###
+DOCLETPATH="-docletpath $HERE/javadoc/doclava.jar"
+DOCLET="-doclet com.google.doclava.Doclava"
+EXTRA1="-hdf project.name \"Eventful\" "
+EXTRA2="-XDignore.symbol.file"
+### doclet: apiviz ###
+#STYLESHEET="-stylesheetfile $HERE/javadoc/stylesheet.css"
+#DOCLETPATH="-docletpath $HERE/javadoc/apiviz.jar"
+#DOCLET="-doclet org.jboss.apiviz.APIviz"
+
+###########################################################
+
+rm -rf $OUTDIR/*
+echo "DOCLETPATH $DOCLETPATH"
+echo "DOCLET $DOCLET"
+echo "STYLESHEET $STYLESHEET"
+echo "OUTDIR $OUTDIR"
+echo "EXTRA1 $EXTRA1"
+read -p "Enter to continue"
+
+
+### module:libeventful ###
+javadoc $(find libeventful/src/main/java/ -type f | grep "\.java") -d $OUTDIR $STYLESHEET $DOCLETPATH $DOCLET $EXTRA1 $EXTRA2
+
+### module:sampleApp ###
+#javadoc $(find sampleApp/src/main/java/ -type f | grep "\.java") -d $OUTDIR $STYLESHEET $DOCLETPATH $DOCLET $EXTRA1 $EXTRA2
+
